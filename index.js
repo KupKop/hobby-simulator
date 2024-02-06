efa7d94c31c45f56b01f1f284233c5f082af8181
KupKop <cervenkajb@gmail.com>
2024-02-06 15:31:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-let firstCard = ""
-let secondCard = ""
-let sum = ""
+//let firstCard = ""
+//let secondCard = ""
+//let sum = ""
 let hasBlackJack = false
 let isAlive = true
-let message = ""
-let gameStart = document.getElementById("button");
-let cardsel = document.getElementById("cards-el");
-let sumel = document.getElementById("sum-el");
-let notifel = document.getElementById("notif-el");
-let messageEl = document.getElementById("message-el");
+//let message = ""
+//let gameStart = document.getElementById("button");
+//let cardsel = document.getElementById("cards-el");
+//let sumel = document.getElementById("sum-el");
+//let sumel = document.querySelector("#sum-el");
+//let notifel = document.getElementById("notif-el");
+//let messageEl = document.getElementById("message-el");
 let newCard = ""
 
 function gameStarter() {
-firstCard = Math.floor((Math.random() * 11) + 2);
-secondCard = Math.floor((Math.random() * 11) + 2);
-sum = firstCard + secondCard
-
+//let gameStart = document.getElementById("button");
+let messageEl = document.getElementById("message-el");
+let firstCard = Math.floor((Math.random() * 11) + 2);
+let secondCard = Math.floor((Math.random() * 11) + 2);
+let sum = firstCard + secondCard
+let message = ""
+let cardsel = document.getElementById("cards-el");
+let notifel = document.getElementById("notif-el");
 
 if (sum <= 20) {
     message = "Chceš kartu mrtko? 🙂"
@@ -30,10 +35,10 @@ if (sum <= 20) {
     notifel = message
     cardsel2 = firstCard + ", " + secondCard
     //document.getElementById("cards-el").innerText = "Cards: " + cardsel;
-    //document.getElementById("sum-el").innerText = "Sum: " + sum;
+    document.getElementById("sum-el").innerText = "Sum: " + sum;
     //document.getElementById("message-el").innerText = notifel;
 
-sumel.innerText = "Sum: " + sum
+//sumel.innerText = "Sum: " + sum
 messageEl.innerText = notifel
 cardsel.innerText = "Cards: " + cardsel2
 }
